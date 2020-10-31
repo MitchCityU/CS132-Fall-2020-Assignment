1ace60cbe8c3bcd4885e44b8ad526b353de4de2b
MitchCityU <fademitch@cityuniversity.edu>
2020-10-30 21:14:16 -0700

    Submission for Module 4

---
+++ Module 4/Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        public static void PrintSomething()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public static int Adding(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum; 
+        }
+
+        public static int Mod(int a, int b)
+        {
+            int modulo = a % b;
+            return modulo; 
+        }
+        static void Main(string[] args)
+        {
+            //PrintSomething(); 
+            //Console.WriteLine(Adding(10, 20));
+            Console.WriteLine(Mod(20,13)); 
+        }
+    }
+}
