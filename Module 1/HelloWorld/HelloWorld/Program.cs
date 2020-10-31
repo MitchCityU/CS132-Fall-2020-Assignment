f117403102a7fe784509cac62afc3378b7bdd6f1
MitchCityU <fademitch@cityuniversity.edu>
2020-10-30 22:13:21 -0700

    Submission for Module 1

---
+++ Module 1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /* Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/____|");  */
+
+            string Name = "Mitch";
+            int Age = 25;
+            bool isFemale = false;
+            float MyFloat = 10.1F; 
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+            Console.WriteLine(MyFloat); 
+        }
+    }
+}
